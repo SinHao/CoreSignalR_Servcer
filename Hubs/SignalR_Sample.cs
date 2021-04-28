798e6aefbcbc34ee1bd21922c1da462d85f49683
SinHao <sos80065@yahoo.com.tw>
2021-04-28 21:48:05 +0800

    using Core 3.1

---
+++ Hubs/SignalR_Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSignalR_Servcer.Hubs
+{
+    public class SignalR_Sample : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task ServerMethod1(string userName, string message)
+        {
+            await this.Clients.All.SendCoreAsync("ClientMethod1", new object[] { userName, message });
+        }
+    }
+}
